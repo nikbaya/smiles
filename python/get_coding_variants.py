c8cf91c3856e36e8f495ccef45f8f26ce1423265
nikbaya <nbaya@broadinstitute.org>
2019-07-15 16:55:22 -0400

    updated plots and code for ld clumping

--- python/get_coding_variants.py
+++ python/get_coding_variants.py
@@ -4,9 +4,11 @@
 Created on Wed Jul  3 10:59:21 2019
 
 get binary annotations for whether a variant is in a gene (i.e. coding variant)
-Uses Brandon Franco's code to read in gencode data
+Uses Brandon Franco's (or Liam Abbott's?) code to read in gencode data
 gencode data: 
+    
 ftp://ftp.ebi.ac.uk/pub/databases/gencode/Gencode_human/release_31/
+
 https://www.gencodegenes.org/human/release_30.html
 
 @author: nbaya
@@ -14,9 +16,9 @@ https://www.gencodegenes.org/human/release_30.html
 
 import hail as hl
 
-wd = '/Users/nbaya/Documents/lab/smiles/data/'
+wd_data = '/Users/nbaya/Documents/lab/smiles/data/'
 
-t = hl.import_table('/Users/nbaya/Downloads/gencode.v31lift37.annotation.gff3',no_header=True,impute=True, comment=('#'))
+t = hl.import_table(wd_data+'gencode.v31lift37.annotation.gff3.gz',no_header=True,impute=True, comment=('#'),force=True)
 #t = hl.import_table('/Users/nbaya/Downloads/gencode.v31lift37.annotation.gtf',no_header=True,impute=True, comment=('#'))
 
                                                                                                                     
@@ -34,19 +36,22 @@ t2 = t2.drop(t2.GFF_Columns, t2.f8, t2.f0, t2.f1, t2.f2, t2.f3, t2.f4, t2.f5, t2
 t2 = t2.key_by(t2.interval)
 
                                                                                                               
-
-#ss0 = hl.import_table(wd+'50_irnt.gwas.imputed_v3.both_sexes.tsv.bgz',impute=True,force_bgz=True)
-ss0 = hl.import_table(wd+'21001_irnt.gwas.imputed_v3.both_sexes.tsv.bgz',impute=True,force_bgz=True)
-#ss0 = hl.import_table(wd+'21001_irnt.gwas.imputed_v3.both_sexes.tsv.bgz',impute=True,force_bgz=True)
-variant = ss0.variant.split(':')
-ss = ss0.filter(hl.is_valid_locus(variant[0], 
-                                  hl.int(variant[1]),
-                                  'GRCh37'))
-locus = ss.variant.split(':')
-ss = ss.annotate(locus = hl.parse_locus(locus[0]+':'+locus[1],reference_genome='GRCh37'))
+filename = '50_irnt.gwas.imputed_v3.both_sexes.tsv.bgz'
+filename = '21001_irnt.gwas.imputed_v3.both_sexes.tsv.bgz'
+filename = 'EUR.IBD.gwas_info03_filtered.assoc.tsv.gz'
+ss0 = hl.import_table(wd_data+filename,impute=True,force=True,types={'chr':hl.tstr})
+if 'variant' in list(ss0.row): 
+    variant = ss0.variant.split(':')
+    ss = ss0.filter(hl.is_valid_locus(variant[0], 
+                                      hl.int(variant[1]),
+                                      'GRCh37'))
+    locus = ss.variant.split(':')
+    ss = ss.annotate(locus = hl.parse_locus(locus[0]+':'+locus[1],reference_genome='GRCh37'))
+elif 'chr' in list(ss0.row) and 'pos' in list(ss0.row):
+    ss = ss0.annotate(locus = hl.locus(contig=ss0.chr,pos=ss0.pos,reference_genome='GRCh37'))
 ss = ss.annotate(coding=hl.is_defined(t2[ss.locus]))
-
-ss.export(wd+'21001_irnt.gwas.imputed_v3.both_sexes.coding.tsv.bgz')
+ss = ss.drop('locus')
+ss.export(wd_data+filename.split('.tsv')[0]+'.coding.tsv'+filename.split('.tsv')[1])
 
 
 
--- python/plot_smiles.py
+++ python/plot_smiles.py
@@ -15,20 +15,31 @@ import matplotlib.pyplot as plt
 wd = "/Users/nbaya/Documents/lab/smiles/"
 
 phen_dict = {'50_irnt.gwas.imputed_v3.both_sexes.coding.tsv.bgz':'Standing height',
-             '21001_irnt.gwas.imputed_v3.both_sexes.coding.tsv.bgz':'BMI'}#,
+             '21001_irnt.gwas.imputed_v3.both_sexes.coding.tsv.bgz':'BMI',
 #             'Mahajan.NatGenet2018b.T2Dbmiadj.European.txt.gz':'T2D'} #this is the edited version of the original data, some unnecessary columns were removed
 #             '2443.gwas.imputed_v3.both_sexes.tsv.bgz':'Diabetes diagnosed'} #Directly from UKB
 #        'pgc.scz.full.2012-04.tsv.gz':'SCZ'} #cases: 36,989, controls: 113,075, total: 150064
-#        'EUR.IBD.gwas_info03_filtered.assoc.tsv.gz':'IBD',#EUR IBD from transethnic ancestry meta-analysis
-#        'EUR.CD.gwas_info03_filtered.assoc.tsv.gz':'CD'}#, #EUR CD from transethnic ancestry meta-analysis
-#        'EUR.UC.gwas_info03_filtered.assoc.tsv.gz':'UC'} #EUR UC from transethnic ancestry meta-analysis
-
+        'EUR.IBD.gwas_info03_filtered.assoc.coding.tsv.gz':'IBD',#EUR IBD from transethnic ancestry meta-analysis
+        'EUR.CD.gwas_info03_filtered.assoc.coding.tsv.gz':'CD', #EUR CD from transethnic ancestry meta-analysis
+        'EUR.UC.gwas_info03_filtered.assoc.coding.tsv.gz':'UC'} #EUR UC from transethnic ancestry meta-analysis
 
 highlight_coding = True
+ld_clumping = True
+ld_window = int(100e3)
 
 for filename, phen in phen_dict.items():
     
     ss0 = pd.read_csv(wd+'data/'+filename, sep='\t',compression='gzip')
+    if 'chr' not in ss0.columns.values:
+        ss0['chr'] = ss0.variant.str.split(':',n=1,expand=True).iloc[:,0]
+    ss0['chr'] = ss0['chr'].astype(str)
+    
+    if ld_clumping:
+        if f'ld_index_{ld_window}' not in ss0.columns.values:
+            ss0[f'ld_index_{ld_window}'] = [f'{entry.chr}-{int(entry.pos/ld_window)}' for id,entry in ss0.iterrows()]
+            ss0.to_csv(wd+'data/'+filename, sep='\t',compression='gzip')
+        ss0 = ss0.loc[ss0.groupby(f'ld_index_{ld_window}')['pval'].idxmin()]
+            
             
     for pval_threshold in [1e-5,5e-8,1e-8]:
         
@@ -58,9 +69,9 @@ for filename, phen in phen_dict.items():
             
             #effect size beta
             fig,ax=plt.subplots(figsize=(6*1.2,4*1.2))
-            if 'coding' in ss.columns.values:
+            if 'coding' in ss.columns.values and highlight_coding:
                 ax.plot(ss[~ss.coding].raf, ss[~ss.coding].rbeta,'.',ms=2,c='#1f77b4') #plot noncoding variants
-                ax.plot(ss[ss.coding].raf, ss[ss.coding].rbeta, 'o',markerfacecolor='none', markeredgewidth=0.5) #plot coding variants
+                ax.plot(ss[ss.coding].raf, ss[ss.coding].rbeta, 'o',markerfacecolor='none', markeredgewidth=0.75) #plot coding variants
                 plt.legend(['non-coding','coding'])
             else:
                 ax.plot(ss.raf, ss.rbeta,'.',ms=2,alpha=1)
@@ -68,47 +79,48 @@ for filename, phen in phen_dict.items():
             plt.ylabel('Estimated effect size')
             plt.title(f'AF vs. Effect Size\nphen: {phen}, n: {n}, pval_threshold: {pval_threshold}'+(f', maf>{maf}' if maf!=0 else ''))
             fig=plt.gcf()
-            if highlight_coding:
-                fig.savefig(wd+"smiles/plots/"+file_prefix+'.raf_effectsize.highlightcoding.png',dpi=600)
-            else:
-                fig.savefig(wd+"smiles/plots/"+file_prefix+'.raf_effectsize.png',dpi=600)
+            suffix = ''
+            if 'coding' in ss.columns.values  and highlight_coding:
+                suffix = '.highlightcoding'
+            if ld_clumping:
+                suffix += f'.ldclumping{ld_window}'
+            fig.savefig(wd+f'smiles/plots/{file_prefix}.raf_effectsize{suffix}.png',dpi=600)
             plt.close()
                 
             #variance explained
             fig,ax=plt.subplots(figsize=(6*1.2,4*1.2))    
-            if 'coding' in ss.columns.values:
+            if 'coding' in ss.columns.values  and highlight_coding:
                 raf = ss[~ss.coding]['raf']
                 rbeta = ss[~ss.coding]['rbeta']
                 raf_c = ss[ss.coding]['raf'] #raf for coding variants
                 rbeta_c = ss[ss.coding]['rbeta'] #raf for coding variants
                 ax.plot(raf, 2*raf*(1-raf)*rbeta**2,'.',ms=2,c='#1f77b4')
-                ax.plot(raf_c, 2*raf_c*(1-raf_c)*rbeta_c**2, 'o',markerfacecolor='none', markeredgewidth=0.5,c='#1f77b4')
+                ax.plot(raf_c, 2*raf_c*(1-raf_c)*rbeta_c**2, 'o',markerfacecolor='none', markeredgewidth=0.75,c='#1f77b4')
                 plt.legend(['non-coding','coding'])
             else:
                 ax.plot(ss.raf, 2*ss.raf*(1-ss.raf)*ss.rbeta**2,'.',ms=2)
             plt.xlabel('Risk allele frequency')
             plt.ylabel('Variance explained')
             plt.title(f'AF vs. Variance Explained\nphen: {phen}, n: {n}, pval_threshold: {pval_threshold}'+(f', maf>{maf}' if maf!=0 else ''))
-            if highlight_coding:
-                fig.savefig(wd+"smiles/plots/"+file_prefix+'.raf_varianceexplained.highlightcoding.png',dpi=600)
-            else:
-                fig.savefig(wd+"smiles/plots/"+file_prefix+'.raf_varianceexplained.png',dpi=600)
+            suffix = ''
+            if 'coding' in ss.columns.values  and highlight_coding:
+                suffix = '.highlightcoding'
+            if ld_clumping:
+                suffix += f'.ldclumping{ld_window}'
+            fig.savefig(wd+f'smiles/plots/{file_prefix}.raf_varianceexplained{suffix}.png',dpi=600)
             plt.close()
     
             #variance explained, colored by chromosome
             fig,ax=plt.subplots(figsize=(6*1.2,4*1.2))
-            if 'chr' not in ss.columns.values:
-                ss['chr'] = ss.variant.str.split(':',n=1,expand=True).iloc[:,0]
-            ss['chr'] = ss['chr'].astype(str)
             colors = plt.rcParams['axes.prop_cycle'].by_key()['color']
-            for i, ch in enumerate(list(range(1,23))+['X']):
-                if 'coding' in ss.columns.values:
+            for i, ch in enumerate([*range(1,23)]+['X']):
+                if 'coding' in ss.columns.values and highlight_coding:
                     raf = ss[(ss.chr==str(ch))&(~ss.coding)]['raf']
                     rbeta = ss[(ss.chr==str(ch))&(~ss.coding)]['rbeta']
                     raf_c = ss[(ss.chr==str(ch))&(ss.coding)]['raf'] #raf for coding variants
                     rbeta_c = ss[(ss.chr==str(ch))&(ss.coding)]['rbeta'] #raf for coding variants
                     ax.plot(raf, 2*raf*(1-raf)*rbeta**2,'.',ms=2,c=colors[i%10])
-                    ax.plot(raf_c, 2*raf_c*(1-raf_c)*rbeta_c**2, 'o',markerfacecolor='none', markeredgewidth=0.5,c=colors[i%10],alpha=0.5)
+                    ax.plot(raf_c, 2*raf_c*(1-raf_c)*rbeta_c**2, 'o',markerfacecolor='none', markeredgewidth=0.75,c=colors[i%10])
                 else:
                     raf = ss[ss.chr==str(ch)]['raf']
                     rbeta = ss[ss.chr==str(ch)]['rbeta']
@@ -116,8 +128,10 @@ for filename, phen in phen_dict.items():
             plt.xlabel('Risk allele frequency')
             plt.ylabel('Variance explained')
             plt.title(f'AF vs. Variance Explained, colored by chromosome\nphen: {phen}, n: {n}, pval_threshold: {pval_threshold}'+(f', maf>{maf}' if maf!=0 else ''))
-            if highlight_coding:
-                fig.savefig(wd+"smiles/plots/"+file_prefix+'.raf_varianceexplained.coloredbychr.highlightcoding.png',dpi=600)
-            else:
-                fig.savefig(wd+"smiles/plots/"+file_prefix+'.raf_varianceexplained.coloredbychr.png',dpi=600)
+            suffix = ''
+            if 'coding' in ss.columns.values  and highlight_coding:
+                suffix = '.highlightcoding'
+            if ld_clumping:
+                suffix += f'.ldclumping{ld_window}'
+            fig.savefig(wd+f'smiles/plots/{file_prefix}.raf_varianceexplained.coloredbychr{suffix}.png',dpi=600)
             plt.close()
