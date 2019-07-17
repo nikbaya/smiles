eea0604519aeb464137833da6e615823078f2412
nikbaya <nbaya@broadinstitute.org>
2019-07-17 07:53:40 -0400

    updated code

--- python/plot_smiles.py
+++ python/plot_smiles.py
@@ -14,25 +14,31 @@ import matplotlib.pyplot as plt
 
 wd = "/Users/nbaya/Documents/lab/smiles/"
 
-phen_dict = {'50_irnt.gwas.imputed_v3.both_sexes.coding.tsv.bgz':'Standing height',
-             '21001_irnt.gwas.imputed_v3.both_sexes.coding.tsv.bgz':'BMI',
+phen_dict = {#'50_irnt.gwas.imputed_v3.both_sexes.coding.tsv.bgz':'Standing height',
+#             '21001_irnt.gwas.imputed_v3.both_sexes.coding.tsv.bgz':'BMI',
 #             'Mahajan.NatGenet2018b.T2Dbmiadj.European.txt.gz':'T2D'} #this is the edited version of the original data, some unnecessary columns were removed
-#             '2443.gwas.imputed_v3.both_sexes.tsv.bgz':'Diabetes diagnosed'} #Directly from UKB
+             '2443.gwas.imputed_v3.both_sexes.coding.tsv.bgz':'Diabetes diagnosed'} #Directly from UKB
 #        'pgc.scz.full.2012-04.tsv.gz':'SCZ'} #cases: 36,989, controls: 113,075, total: 150064
-        'EUR.IBD.gwas_info03_filtered.assoc.coding.tsv.gz':'IBD',#EUR IBD from transethnic ancestry meta-analysis
-        'EUR.CD.gwas_info03_filtered.assoc.coding.tsv.gz':'CD', #EUR CD from transethnic ancestry meta-analysis
-        'EUR.UC.gwas_info03_filtered.assoc.coding.tsv.gz':'UC'} #EUR UC from transethnic ancestry meta-analysis
+#        'EUR.IBD.gwas_info03_filtered.assoc.coding.tsv.gz':'IBD',#EUR IBD from transethnic ancestry meta-analysis
+#        'EUR.CD.gwas_info03_filtered.assoc.coding.tsv.gz':'CD', #EUR CD from transethnic ancestry meta-analysis
+#        'EUR.UC.gwas_info03_filtered.assoc.coding.tsv.gz':'UC'} #EUR UC from transethnic ancestry meta-analysis
 
 highlight_coding = True
-ld_clumping = True
+ld_clumping = False
 ld_window = int(100e3)
 
 for filename, phen in phen_dict.items():
+    print(f'Starting phen {phen}, using file {filename}')
     
     ss0 = pd.read_csv(wd+'data/'+filename, sep='\t',compression='gzip')
+    
     if 'chr' not in ss0.columns.values:
         ss0['chr'] = ss0.variant.str.split(':',n=1,expand=True).iloc[:,0]
     ss0['chr'] = ss0['chr'].astype(str)
+    
+    if 'pos' not in ss0.columns.values:
+        ss0['pos'] = ss0.variant.str.split(':',n=2,expand=True).iloc[:,1]
+    ss0['pos'] = ss0['pos'].astype(int)
     
     if ld_clumping:
         if f'ld_index_{ld_window}' not in ss0.columns.values:
@@ -77,7 +83,8 @@ for filename, phen in phen_dict.items():
                 ax.plot(ss.raf, ss.rbeta,'.',ms=2,alpha=1)
             plt.xlabel('Risk allele frequency')
             plt.ylabel('Estimated effect size')
-            plt.title(f'AF vs. Effect Size\nphen: {phen}, n: {n}, pval_threshold: {pval_threshold}'+(f', maf>{maf}' if maf!=0 else ''))
+            plt.title(f'AF vs. Effect Size\nphen: {phen}, n: {n}, pval threshold: {pval_threshold}'
+                      +(f', maf>{maf}' if maf!=0 else '')+(f', ld block: {int(ld_window/1000)}kb' if ld_clumping else ''))
             fig=plt.gcf()
             suffix = ''
             if 'coding' in ss.columns.values  and highlight_coding:
@@ -101,7 +108,8 @@ for filename, phen in phen_dict.items():
                 ax.plot(ss.raf, 2*ss.raf*(1-ss.raf)*ss.rbeta**2,'.',ms=2)
             plt.xlabel('Risk allele frequency')
             plt.ylabel('Variance explained')
-            plt.title(f'AF vs. Variance Explained\nphen: {phen}, n: {n}, pval_threshold: {pval_threshold}'+(f', maf>{maf}' if maf!=0 else ''))
+            plt.title(f'AF vs. Variance Explained\nphen: {phen}, n: {n}, pval threshold: {pval_threshold}'
+                      +(f', maf>{maf}' if maf!=0 else '')+(f', ld block: {int(ld_window/1000)}kb' if ld_clumping else ''))
             suffix = ''
             if 'coding' in ss.columns.values  and highlight_coding:
                 suffix = '.highlightcoding'
@@ -127,7 +135,8 @@ for filename, phen in phen_dict.items():
                     ax.plot(raf, 2*raf*(1-raf)*rbeta**2,'.',ms=2)
             plt.xlabel('Risk allele frequency')
             plt.ylabel('Variance explained')
-            plt.title(f'AF vs. Variance Explained, colored by chromosome\nphen: {phen}, n: {n}, pval_threshold: {pval_threshold}'+(f', maf>{maf}' if maf!=0 else ''))
+            plt.title(f'AF vs. Variance Explained, colored by chromosome\nphen: {phen}, n: {n}, pval threshold: {pval_threshold}'
+                      +(f', maf>{maf}' if maf!=0 else '')+(f', ld block: {int(ld_window/1000)}kb' if ld_clumping else ''))
             suffix = ''
             if 'coding' in ss.columns.values  and highlight_coding:
                 suffix = '.highlightcoding'
